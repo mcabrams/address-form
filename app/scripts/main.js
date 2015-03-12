f450bf13e148f8156b5f76c09ea66ffd498b497e
Matthew Abrams <mcabrams1@gmail.com>
2015-03-11 20:32:25 -0500

    Iterate over input fields and append to formData
    Before any validation code, I plan on getting this to the point where we
    simply output what the user inputs w/o any checking of it. Getting
    things to this point (data is represented in object), is the main chunk
    of that work - actually displaying the data comes next.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,38 @@
 ( function ($, window, document, undefined ) {
   'use strict';
 
-  $( document ).ready( function () {
-    console.log('hello world');
+  $( document ).ready(function () {
+
+    var INPUT_NAME_LOOKUP = {
+      'addressLine1': 'Address Line 1',
+      'addressLine2': 'Address Line 2',
+      'city': 'City',
+      'firstName': 'First Name',
+      'lastName': 'Last Name',
+      'state': 'State',
+      'zip': 'Zip'
+    };
+
+    // Form submit handler
+    $('#address-form').submit(function() {
+      var formData = {};
+
+      // Iterate over inputs and add info to formData object
+      $( this ).children().filter('input:not([type=submit])').each(function() {
+        var $input = $( this );
+        formData[$input.prop('name')] = $input.val();
+        console.log(INPUT_NAME_LOOKUP[$( this ).prop('name')] + ': ' + $( this ).val());
+      });
+
+      console.log(formData);
+
+      event.preventDefault();
+    });
+
+    function displayFormData(formData) {
+      // Todo
+    }
+
   });
 
 })( jQuery, window, document );
