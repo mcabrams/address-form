e5477dfd8f5619cc6f6386ca11440259e219378a
Matthew Abrams <mcabrams1@gmail.com>
2015-03-14 16:56:47 -0500

    Address autocompletion via google
    I decided to use Google's Maps API to implement autocomplete for the
    address box. It has the benefit of generally requiring less typing,
    provides the user with immediate reapproval that they're entering their
    address correctly, and overall, provides a better UX. That said, it
    comes with some drawbacks: we now have an external service that we're
    depending on - that said, if the API is down, nothing breaks,
    it just diminishes the UX and the user has to fill out the form as they
    normally would.
    
    Google doesn't seem to have the best support/data for Unit numbers and other "subpremise"
    information. This means Address Line 2 can be left empty, which brings
    up an interesting UX conundrum since it's not required, but
    could definitely be an important piece of information that gets left out.
    I think I'll try to address this down the line.
    
    The "Powered by Google" makes things feel a little "cookie cut,"
    but it's required by the TOS.
    
    I consider SmartyStreets as another API, but after
    surveying both API offerings and documentation, I decided to
    go with the better established one. I am curious if SmartyStreets would
    be better at dealing with the subpremise info.
    
    Finally, there's much more JS code now - I plan on modularizing it so
    it's easier to follow, and will probably refactor it, given time.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,27 +4,61 @@
 
   $( document ).ready(function () {
 
-    onDocumentReady();
-
-    var INPUT_NAME_LOOKUP = {
-      'addressLine1': 'Address Line 1',
-      'addressLine2': 'Address Line 2',
-      'city': 'City',
-      'firstName': 'First Name',
-      'lastName': 'Last Name',
-      'state': 'State',
-      'zip': 'Zip'
+    var FORM_COMPONENTS = {
+      'address-line1-input': {
+        textToDisplay: 'Address Line 1',
+        googleType: 'street_number',
+        googleNameLength: 'long_name',
+        htmlId: 'address-line1-input'
+      },
+      'address-line2-input': {
+        textToDisplay: 'Address Line 2',
+        googleType: 'subpremise',
+        googleNameLength: 'long_name',
+        htmlId: 'address-line2-input'
+      },
+      'city-input': {
+        textToDisplay: 'City',
+        googleType: 'locality',
+        googleNameLength: 'long_name',
+        htmlId: 'city-input'
+      },
+      'first-name-input': {
+        textToDisplay: 'First Name',
+        googleType: null,
+        googleNameLength: null,
+        htmlId: 'first-name-input'
+      },
+      'last-name-input': {
+        textToDisplay: 'Last Name',
+        googleType: null,
+        googleNameLength: null,
+        htmlId: 'last-name-input'
+      },
+      'state-input': {
+        textToDisplay: 'State',
+        googleType: 'administrative_area_level_1',
+        googleNameLength: 'short_name',
+        htmlId: 'state-input'
+      },
+      'zip-input': {
+        textToDisplay: 'Zip',
+        googleType: 'postal_code',
+        googleNameLength: 'short_name',
+        htmlId: 'zip-input'
+      }
     };
 
-    // Form submit handler
+    onDocumentReady();
+
+    // Form submission handler
     $('#address-form').submit(function() {
       var formData = {};
 
       // Iterate over inputs and add info to formData object
-      $( this ).children().filter('input:not([type=submit])').each(function() {
+      $( this ).find('input:not([type=submit])').each(function() {
         var $input = $( this );
-        formData[$input.prop('name')] = $input.val();
-        console.log(INPUT_NAME_LOOKUP[$( this ).prop('name')] + ': ' + $( this ).val());
+        formData[$input.attr('id')] = $input.val();
       });
 
       displayFormData(formData);
@@ -34,17 +68,82 @@
 
     function onDocumentReady() {
 
-      // If jvFloat is available, initialize the form
+      // If jvFloat is available, initialize the fields with it
       if ($.isFunction(jQuery.fn.jvFloat)) {
         $('#address-form').children().jvFloat();
       }
+
+      // If google places API available, set up address autocompletion
+      if (typeof google !== 'undefined') {
+
+        var autocompleteInput = document.getElementById('address-line1-input');
+
+        var northAmericaBounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(83.162102, -52.233040),
+            new google.maps.LatLng(5.499550, -167.276413)
+          );
+
+        var options = {
+          bounds: northAmericaBounds,
+          types: ['geocode']
+        };
+
+        var autocomplete = new google.maps.places.Autocomplete(autocompleteInput, options);
+
+        // When the user selects an address from the dropdown,
+        // populate the address fields in the form.
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+          fillInAddress();
+        });
+
+        function fillInAddress() {
+          // Get the place details from the autocomplete object.
+          var place = autocomplete.getPlace();
+
+          _.each(FORM_COMPONENTS, function(fieldValue, fieldName){
+            if (fieldName !== 'first-name-input' && fieldName !== 'last-name-input') {
+              document.getElementById(fieldName).value = '';
+              document.getElementById(fieldName).disabled = false;
+            }
+          });
+
+          var streetNumber;
+          var route;
+
+          // Get each component of the address from the place details
+          // and fill the corresponding field on the form.
+          _.each(place.address_components, function(addressComponent){
+            var addressType = addressComponent.types[0];
+
+            var addressInfoLookup = _.where(FORM_COMPONENTS, {googleType: addressType})[0];
+
+            if (addressType === 'street_number') {
+              streetNumber = addressComponent.long_name;
+              return;
+            }
+
+            if (addressType === 'route') {
+              route = addressComponent.long_name;
+              return;
+            }
+
+            if (typeof addressInfoLookup !== 'undefined') {
+              var val = addressComponent[addressInfoLookup.googleNameLength];
+              document.getElementById(addressInfoLookup.htmlId).value = val;
+            }
+          });
+
+          document.getElementById('address-line1-input').value = streetNumber + ' ' + route;
+        }
+      }
+
     }
 
     function displayFormData(formData) {
       var displayedUl = $( '<ul/>' );
 
-      _.each(formData, function(fieldValue ,fieldName){
-        displayedUl.append($('<li>' + INPUT_NAME_LOOKUP[fieldName] + ': ' + fieldValue + '</li>'));
+      _.each(formData, function(fieldValue, fieldName){
+        displayedUl.append($('<li>' + FORM_COMPONENTS[fieldName].textToDisplay + ': ' + fieldValue + '</li>'));
       });
 
       $('#address-summary').append(displayedUl);
